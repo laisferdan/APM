7dbd1a495bd1bf93f0ee983a2d854bf93e224558
Laís Fernandes Daniel <laisfd@hotmail.com>
2023-11-15 11:45:36 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from "./pages/product-list/product-list.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet]
+})
+export class AppComponent {
+  pageTitle: string = 'Application Product Management';
+}
