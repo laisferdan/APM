5f56ee17d84346709d46468b5ea527c90cc9192a
Laís Fernandes Daniel <laisfd@hotmail.com>
2023-11-19 13:08:49 -0300

    feat: create  and apply product service

--- src/app/features/pages/product-list/product-list.component.ts
+++ src/app/features/pages/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../../shared/shared.module';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -27,31 +28,13 @@ export class ProductListComponent implements OnInit {
   }
 
   public filteredProducts: IProduct[] = [];
-  public products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
+  public products: IProduct[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    console.log('');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   public onRatingClick(message: string): void {
